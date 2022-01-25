c1a7c084a88d38b29efe7cc9288491408e7f0647
navintkr <navintkr@gmail.com>
2022-01-25 11:04:06 -0600

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,33 @@
 import { Component } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { map, delay } from "rxjs/operators";
+import axios from 'axios';
 
 @Component({
   selector: 'app-root',
-  template: `<div>Hello {{value}}</div>`,
+  template: `<div>Hello {{value}}</div><div>Progress: </div><div>Response:<pre></pre></div><button (click)="fetchData()"> Get </button>`,
 })
 export class AppComponent {
   value = 'World';
+
+  private apiURL = "https://python-appinsights-check.azurewebsites.net/api/HttpTrigger1?name=naveen";
+  public message: string = "Uninitialized";
+  public response;
+
+  constructor(private httpClient: HttpClient) {}
+
+  
+  async fetchData() {
+    this.message = "Fetching..";
+    this.response = "";
+    this.response = await axios(this.apiURL, {
+      method: "get",
+      withCredentials: true,
+      headers: {
+          "Accept": "application/json",
+          "Content-Type": "application/json",                
+      }
+    });
+    this.message = "Fetched";
+  }
 }
